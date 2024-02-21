c655a5f9efc5c885bc97596baf0abfb2ae157634
Tanner Helms <a02267747@usu.edu>
2024-02-21 11:59:45 -0700

    added in snippits

---
+++ client/src/componets/test/test.jsx
@@ -0,0 +1,12 @@
+import classes from './test.module.css'
+import { useEffect, useState } from 'react';
+
+function Test() {
+    return (
+        <section className={classes.home}>
+
+        </section>
+    )
+}
+
+export default Test;
--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,37 +1,36 @@
 import Navbar from "../../componets/navbar/navbar";
 import classes from "./home.module.css";
-import DailyNotification from "../../componets/daily_notification/daily_notification"
-function Home() {
-    return (
+import DailyNotification from "../../componets/daily_notification/daily_notification";
 
-        <div className={classes.container}>
+function Home() {
+
+    return (
+        <section className={classes.container}>
 
             {/* Navigation Bar */}
             <Navbar title="Home"></Navbar>
 
             {/* Container for Budget */}
-            <div className={classes.budget}>
+            <section className={classes.budget}>
                 <h2>Good Morning,</h2>
                 <p className="label">Summary of your budget</p>
                 <hr />
-                <div className={classes.balance}>
-                    <div>
+                <section className={classes.balance}>
+                    <section>
                         <p>Balance</p>
                         <p>$65.32</p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <p>Daily budget</p>
                         <p>$25</p>
-                    </div>
-                </div>
-            </div>
-
+                    </section>
+                </section>
+            </section>
             {/* Container for Daily Notifications */}
             <p className="label">Daily Notifications</p>
             <DailyNotification title="Be Yourself" body="Authenticity is attractive. Don't try to be someone you're not just to.." />
             <DailyNotification title="Smell Good" body="Take a moment in your morning routine to put on some..." />
-        </div>
-
+        </section>
     )
 }
 
