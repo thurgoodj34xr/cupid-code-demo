7b98bfd73c117b09cc826503896b490f2870a8e7
Tanner Helms <a02267747@usu.edu>
2024-03-02 18:13:50 -0700

    Notification on purchases

--- client/src/hook/purchases.js
+++ client/src/hook/purchases.js
@@ -1,11 +1,15 @@
 import * as Api from "./api"
 async function PurchaseHistory(userId, context, setState) {
-    var {purchases} = await Api.PostWithAuth(
+    var resp = await Api.PostWithAuth(
       "/getPurchaseHistory",
       { userId },
       context
     );
-    setState(purchases)
+    if (resp.error) {
+      context.sendNotification(`Error: ${resp.error}`)
+    } else {
+      setState(purchases)
+    }
   }
 
   export default PurchaseHistory;
