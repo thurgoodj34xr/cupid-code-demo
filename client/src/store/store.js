0bd95be36e00d7c0ee0871f58140d746aebb0274
TannerHelms <tannerhelms01@gmail.com>
2024-03-21 21:57:55 -0600

    dev tools

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,7 +5,10 @@ import authReducer from "./auth_slice"
 const store = configureStore({
     reducer: {
         auth: authReducer
-    }
+    },
+    devTools: true,
 })
+
+
 
 export default store;
